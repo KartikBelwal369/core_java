6d2e32535e993f801f8e8d1cc54f66aac489c24b
KartikBelwal369 <110006442+KartikBelwal369@users.noreply.github.com>
2022-07-26 13:31:36 +0530

    JAVA ASSIGNMENT 1
    TEST UPLOADED BY 2006912

---
+++ Test.java
@@ -0,0 +1,77 @@
+//QUESTION 1:
+//ANSWER 1:
+
+public class Test {
+
+    public static void main(){
+        Test obj=new Test();
+        obj.hobby();
+    }
+    void hobby(){
+        String name="Kartik Belwal";
+                int age=19;
+                System.out.println("Name="+name);
+                System.out.println("Age="+age);
+    }
+}
+
+
+
+//QUESTION 2:
+//ANSWER 2:
+
+/* 1 : Class name:
+    Test
+    Fun
+
+2 : Function name:
+    test
+    Fun()
+
+3 : Variable:
+    numPlay
+    var
+
+4 : Static variable:
+    NUM
+    PLAY_GAME
+*/
+
+
+/*QUESTION 3:
+  ANSWER 3:
+
+
+1. .java file : Every code written by a programmer is compressed in this file. And hence after its compilation the main
+                file runs.
+    .class file : This is the compiled version of file which can be executed.
+
+2. Java is not completely oop`s language because java contains many primitive datatypes  which are (int char double bool)
+   which are not object and hence there are wrapper classes so java cannot be a pure object oriented class.
+
+3: EXAMPLE IF NR-WA :
+        void test(int a,int b){
+        System.out.println("MyName");
+        }
+ */
+
+
+
+/*QUESTION 5:
+  ANSWER 5:
+
+
+  Wrapper classes : These are the clases that are used to convert primitive datatypes (int , char , fload , bool , etc)
+                    into corresponding objects.
+                    Java Wrapper Class :
+
+                    Primitive type                           Wrapper class
+                    int                                      Integer
+                    bool                                     Bool
+                    char                                     Character
+                    double                                   Double
+ */
+
+
+
+
