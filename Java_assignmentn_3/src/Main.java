554c09bba834c8e41d6e414a94d02335370485d9
KartikBelwal369 <110006442+KartikBelwal369@users.noreply.github.com>
2022-08-09 14:04:16 +0530

    Name : kartik belwal
    URN : 2006912
    Assignment 3

---
+++ Java_assignmentn_3/src/Main.java
@@ -0,0 +1,90 @@
+//Question 1 :
+//Answer 1 :
+
+/* Encapsulation : Encapsulation is a key function of Object oriented programming.hence it refers to the bundling
+                   of fields and method inside single a class.In this we don't provide direct access to the user
+                   we only provide a private id by which user cannot change the main content of the given id.
+
+
+
+class Person {
+private int age;   //private age.
+public int getAge () {   //getting method
+return age;
+}
+public void setAge(int age){
+this.age = age;
+}
+}
+
+public class Main {
+    public static void main(String[] args) {
+    Person p1 = new Person();
+    p1.setAge(19);
+    System.out.println("My age is " = + p1.getAge());
+    }
+}
+
+
+
+Question 2 :
+Answer 2 :
+Polymorphism : It is the technique by which we create methods() which work different for different class.
+Example :
+*/
+
+
+
+//Question 3 :
+//Answer 3 :
+/* 1 : Access specifiers : The access specifiers provides accessiblity to class and methods they are also known as acess
+                           modifiers.There are four types of access specifiers :
+
+                           a : public
+                           b : default
+                           c : private
+                           d : protected
+
+
+ 2: Packages : Packages are used to store the files that we have created in our program.
+                  So if we are creating a package of package com.company then here this package defines that
+                  the classes which we are making into our program that recides in a package names as package com.
+                  company.*/
+                  /*Example :
+                          package com.company;
+                  import java.util.Scanner;
+                  public class Main {
+                  public static void main (string args[]){
+                  System.out.println("Taking input from the user");
+                  Scanner sc = new Scanner(System.in);
+                  System.out.println("Marks in maths");
+                  int maths = sc.nextInt();
+                  System.out.println("Marks in physics");
+                  int physics = sc.nextInt();
+                  System.out.println("Marks in hindi");
+                  int hindi = sc.nextOInt();
+                  int percentage = (maths + physics + hindi)*100/300;
+                  System.out.println(percentage);
+
+                  }
+
+                  }
+
+
+
+3 : Final keyword : The final keyword is a keyword which is non accessible by the user
+                    It restricts the user to make any change in class and method.
+
+                   */
+
+
+
+
+/*Question 5 :
+Answer 5 :
+
+ */
+
+
+
+
